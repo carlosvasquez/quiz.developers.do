b3f70d4f5b4a756ec25cd93c1516d2e9944116be
Carlos Vásquez Polanco <carlosslim@gmail.com>
2014-04-16 23:18:31 -0400

    Agregando mètodos para retornar la lista de cuestionarios generales y por usuario

---
+++ src/main/java/org/devdom/quiz/model/bean/QuizController.java
@@ -0,0 +1,57 @@
+package org.devdom.quiz.model.bean;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import org.devdom.quiz.model.dao.QuizDao;
+import org.devdom.quiz.model.dto.Quiz;
+
+/**
+ *
+ * @author Carlos Vasquez Polanco
+ */
+@ManagedBean
+@SessionScoped
+public class QuizController implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    private final FacesContext facesContext = FacesContext.getCurrentInstance();
+    private final HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+    
+    private static final Logger LOG = Logger.getLogger(QuizController.class.getName());
+    
+    private Quiz quiz;
+    private final QuizDao quizDao = new QuizDao();
+
+    private void reset(){
+        quiz = null;
+    }
+    
+    public List<Quiz> getAllQuiz(){
+        try {
+            return quizDao.findAllQuiz();
+        } catch (Exception ex) {
+            Logger.getLogger(QuizController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public List<Quiz> getAllQuizByUserId(long userId){
+        try {
+            return quizDao.findAllQuizByUserId(userId);
+        } catch (Exception ex) {
+            Logger.getLogger(QuizController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public List<Quiz> getList(){
+        return null;
+    }
+    
+}
